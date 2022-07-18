125fc0a8b2187b86da69107ca0ae9ae3a90e94b7
xcjthu <826983264@qq.com>
2022-07-18 08:07:11 +0000

    modify

--- distribute.py
+++ distribute.py
@@ -12,132 +12,163 @@ from model_center.utils import print_inspect
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, Dataset
+from tokenizers import Tokenizer
+from transformers import PreTrainedTokenizerFast
 import pandas as pd
-import json
+import json,os
 from tqdm import tqdm
+from arguments import get_args
+from torch.utils.tensorboard import SummaryWriter
 
-from preprocess.prepare_dataset_SamVal import get_train_dataset, get_test_dataset
-from preprocess.prepare_dataset_3A import get_3A_dataset
-ROBERTA_MODEL = 'roberta-base'
-PADDING_LEN = 512
-
-log_iter = 50
-label_num = 2
-epochs = 30
-batch_size = 32
-learning_rate = 1e-5
-warm_up_ratio = 0.1
-
-PATH_TO_DATASET = 'data'
-BERT_PATH = '/data0/private/caohanwen/OpenSoCo/checkpoint/reddit_twitter/checkpoint-46499.pt'
-CHECKPOINT_PATH = 'saved_models/model.pt'
-SAVED_PATH = 'saved_models/valid_acc.csv'
+from prepare_dataset_Incivility import get_incivility_dataset
+from prepare_dataset_SamVal import get_train_dataset, get_test_dataset
+from prepare_dataset_3A import get_3A_dataset
+from prepare_dataset_Dreaddit import get_Dreaddit_dataset
+from prepare_dataset_AGNews import get_AGNews_dataset
+from prepare_dataset_Article_Bias import get_Article_Bias_dataset
+# log_iter = 50
+# label_num = 2
+# epochs = 30
+# batch_size = 32
+# learning_rate = 1e-5
+# warm_up_ratio = 0.1
 
 # bmt.print_rank("torch version", torch.__version__)
 # bmt.print_rank(torch.cuda.get_arch_list())
 
+def initialize():
+    # get arguments
+    args = get_args()
+    # init bmp 
+    bmt.init_distributed(seed = args.seed, loss_scale_factor = 2, loss_scale_steps = 1024)
+    # init save folder
+    if args.save != None:
+        os.makedirs(args.save, exist_ok=True)
+    return args
+
+args = initialize()
+print(args)
+if bmt.rank() == 0:
+    writer = SummaryWriter(os.path.join(args.save, 'tensorborads'))
+
+bmt.print_rank(f"Local rank:{bmt.rank()} | World size:{bmt.world_size()}")
+temp_texts, temp_labels = get_train_dataset() # 这里的temp_texts, temp_labels 就是两个同样长度的list，前者是raw text，后者是label（0，1，2）
+test_texts, test_labels = get_test_dataset() #test_texts, test_labels 同上
+#temp_texts, temp_labels = get_incivility_dataset('data/Reddit_Incivility/incivility_coded_0302.train.json')
+#test_texts, test_labels = get_incivility_dataset('data/Reddit_Incivility/incivility_coded_0302.test.json')
+# temp_texts, temp_labels = get_3A_dataset('data/SemEval3A/train_3A.txt', do_preprocess = True)
+# test_texts, test_labels = get_3A_dataset('data/SemEval3A/gold_test_3A.txt', do_preprocess =True)
+# temp_texts, temp_labels = get_Dreaddit_dataset('data/Dreaddit/dreaddit-train.csv')
+# test_texts, test_labels = get_Dreaddit_dataset('data/Dreaddit/dreaddit-test.csv')
+#temp_texts, temp_labels, test_texts, test_labels= get_Article_Bias_dataset('data/Article-Bias-Prediction/jsons')
+#temp_texts, temp_labels = get_AGNews_dataset('data/AGNews/train.csv')
+#test_texts, test_labels = get_AGNews_dataset('data/AGNews/test.csv')
+
+
+assert len(set(temp_labels))== len(set(test_labels))
+label_num = len(set(test_labels))
+
 class RobertaModel(torch.nn.Module):
     def __init__(self, config):
         super().__init__()
-        # self.roberta = Roberta.from_pretrained(ROBERTA_MODEL) # load roberta from pretrained
-        self.roberta = Roberta(config)
-        bmt.load(self.roberta, BERT_PATH)
-        # print_inspect(self.roberta, "*")
+        if args.checkpoint is not None:
+            bmt.print_rank(f"Initializing bert from pretrained {args.checkpoint}...")
+            self.roberta = Roberta.from_pretrained(args.checkpoint) # load roberta from pretrained
+        else: 
+            assert args.load is not None
+            bmt.print_rank(f"Initializing bert from our model path {args.load}...")
+            self.roberta = Roberta(config)
+            bmt.load(self.roberta, args.load)
+        print_inspect(self.roberta, "*")
         self.dense = Linear(config.dim_model, label_num)
         bmt.init_parameters(self.dense) # init dense layer
 
+    def reload(self, config):
+        super().__init__()
+        if args.checkpoint is not None:
+            bmt.print_rank(f"Initializing bert from pretrained {args.checkpoint}...")
+            self.roberta = Roberta.from_pretrained(args.checkpoint) # load roberta from pretrained
+        else: 
+            assert args.load is not None
+            bmt.print_rank(f"Initializing bert from our model path {args.load}...")
+            self.roberta = Roberta(config)
+            bmt.load(self.roberta, args.load)
+        print_inspect(self.roberta, "*")
+        self.dense = Linear(config.dim_model, label_num)
+        bmt.init_parameters(self.dense) # init dense layer 
+
     def forward(self, input_ids, attention_mask):
         pooler_output = self.roberta(input_ids=input_ids, attention_mask=attention_mask).pooler_output
-        x = self.dense(pooler_output)
-        return x
+        logits = self.dense(pooler_output)
+        return logits
 
+if args.checkpoint is not None:
+    config = RobertaConfig.from_pretrained(args.checkpoint)
+else:
+    config = RobertaConfig.from_json_file(args.model_config)
 
-config = RobertaConfig.from_pretrained(ROBERTA_MODEL)
 model = RobertaModel(config)
 
-bmt.print_rank("Prepare dataset...")
-bmt.print_rank(f"local rank:{bmt.rank()}, world size:{bmt.world_size()}")
-# temp_texts, temp_labels = get_train_dataset() # 这里的temp_texts, temp_labels 就是两个同样长度的list，前者是raw text，后者是label（0，1，2）
-# test_texts, test_labels = get_test_dataset() #test_texts, test_labels 同上
-
-temp_texts, temp_labels = get_3A_dataset('data/SemEval3A/train_3A.txt', do_preprocess = True)
-test_texts, test_labels = get_3A_dataset('data/SemEval3A/gold_test_3A.txt', do_preprocess =True)
 train_texts, val_texts, train_labels, val_labels = train_test_split(
     temp_texts,
     temp_labels,
-    random_state = 2022, 
-    test_size=0.1, 
-    stratify = temp_labels
-) # split train and val
-bmt.print_rank(f"train, val and test size is {len(train_labels)}, {len(val_labels)}, {len(test_labels)}")
-# tokenizer = RobertaTokenizer.from_pretrained(ROBERTA_MODEL)
-from tokenizers import Tokenizer
-from transformers import PreTrainedTokenizerFast
+    random_state = 2020, 
+    test_size=0.1,
+    stratify = temp_labels) # split train and val
+bmt.print_rank(f"Train size: {len(train_labels)} | Val size: {len(val_labels)} | Test size: {len(test_labels)}")
+bmt.print_rank(f"{label_num} class classification...")
 
-
-tokenizer_obj = Tokenizer.from_file('/root/bm_train_codes/tokenizer/tokenizer_.json')
-tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer_obj)
-tokenizer.pad_token = '<pad>'
-tokenizer.eos_token = '</s>'
-tokenizer.sep_token = '<s>'
-tokenizer.mask_token = '<mask>'
+if args.checkpoint:
+    tokenizer = RobertaTokenizer.from_pretrained(args.checkpoint)
+else:
+    tokenizer_obj = Tokenizer.from_file(args.tokenizer)
+    tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer_obj)
+    tokenizer.pad_token = '<pad>'
+    tokenizer.eos_token = '</s>'
+    tokenizer.sep_token = '<s>'
+    tokenizer.unk_token = '<unk>'
+    tokenizer.mask_token = '<mask>'
 
 tokens_train = tokenizer.batch_encode_plus(
     train_texts,
-    max_length = PADDING_LEN,
+    max_length = args.padding_len,
     padding='max_length',
-    truncation=True
+    truncation=True,
+    add_special_tokens = True,
 )
 
 tokens_val = tokenizer.batch_encode_plus(
     val_texts,
-    max_length = PADDING_LEN,
+    max_length = args.padding_len,
     padding='max_length',
-    truncation=True
+    truncation=True,
+    add_special_tokens = True,
 )
 
 tokens_test = tokenizer.batch_encode_plus(
     test_texts,
-    max_length = PADDING_LEN,
+    max_length = args.padding_len,
     padding='max_length',
-    truncation=True
+    truncation=True,
+    add_special_tokens = True,
 )
 
-train_data = TensorDataset(torch.tensor(tokens_train['input_ids']).cuda(), \
-    torch.tensor(tokens_train['attention_mask']).cuda(), \
-    torch.tensor(train_labels).cuda())
-val_data = TensorDataset(torch.tensor(tokens_val['input_ids']).cuda(), \
-    torch.tensor(tokens_val['attention_mask']).cuda(), \
-    torch.tensor(val_labels).cuda())
-test_data = TensorDataset(torch.tensor(tokens_test['input_ids']).cuda(), \
-    torch.tensor(tokens_test['attention_mask']).cuda(), \
-    torch.tensor(test_labels).cuda())
+train_data = TensorDataset(torch.tensor(tokens_train['input_ids']), \
+    torch.tensor(tokens_train['attention_mask']), \
+    torch.tensor(train_labels))
+val_data = TensorDataset(torch.tensor(tokens_val['input_ids']), \
+    torch.tensor(tokens_val['attention_mask']), \
+    torch.tensor(val_labels))
+test_data = TensorDataset(torch.tensor(tokens_test['input_ids']), \
+    torch.tensor(tokens_test['attention_mask']), \
+    torch.tensor(test_labels))
 
-train_dataloader = DistributedDataLoader(train_data, batch_size = batch_size, shuffle = True)
-val_dataloader = DistributedDataLoader(val_data, batch_size = batch_size, shuffle = False)
-test_dataloader = DistributedDataLoader(test_data, batch_size = batch_size, shuffle = False)
-# splits = ['val', 'train', 'test']
-# dataset = {}
-# # 手动load test json
-# for text, label in zip(train_texts, train_labels):
-#     dataset['train'] = DATASET()
-# test_path = f"{PATH_TO_DATASET}/BoolQ/test.jsonl"
-# with open(test_path, encoding='utf8') as f:
-#     lines = f.readlines()
-#     for i, raw_row in enumerate(lines):
-#         row = json.loads(raw_row)
-#         label = 1 if row["label"]==True else 0
-#         text_a = row['passage']
-#         text_b = row['question']
-#         template = (f'{text_a}. {text_b}',)
-
-#         dataset['test'] = DATASET['BoolQ'](PATH_TO_DATASET, split, bmt.rank(), bmt.world_size(), tokenizer, max_encoder_length = PADDING_LEN).make_input(tokenizer, template, PADDING_LEN, label)
-
-# train_dataloader = DistributedDataLoader(dataset['train'], batch_size = batch_size, shuffle=True)
-# val_dataloader = DistributedDataLoader(dataset['val'], batch_size=batch_size, shuffle=False)
+train_dataloader = DistributedDataLoader(train_data, batch_size = args.batch_size, shuffle = True)
+val_dataloader = DistributedDataLoader(val_data, batch_size = args.batch_size, shuffle = False)
+test_dataloader = DistributedDataLoader(test_data, batch_size = args.batch_size, shuffle = False)
 
 # optimizer and lr-scheduler
-total_step = (len(train_dataloader)) * epochs
+total_step = (len(train_dataloader)) * args.epochs
 optimizer = bmt.optim.AdamOptimizer(model.parameters(),lr = 1e-5, betas=(0.9,0.98))
 # optimizer = bmt.optim.AdamOffloadOptimizer(model.parameters())
 # lr_scheduler = bmt.lr_scheduler.Linear(
@@ -152,21 +183,29 @@ def fine_tune():
     best_valid_acc = 0
     valid_acc_list = []
     early_stopping = 0
-    for epoch in range(epochs):
+    global_step = 0
+    for epoch in range(args.epochs):
         bmt.print_rank("Epoch {} begin...".format(epoch + 1))
         model.train()
         for step, data in enumerate(train_dataloader):
+            global_step += 1
             input_ids, attention_mask, labels = data
+            # load to cuda
+            input_ids = input_ids.cuda()
+            attention_mask = attention_mask.cuda()
+            labels = labels.cuda()
             optimizer.zero_grad()
             logits = model(input_ids, attention_mask)
             loss = loss_func(logits.view(-1, logits.shape[-1]), labels.view(-1))
             global_loss = bmt.sum_loss(loss).item()
+            if bmt.rank() == 0:
+                writer.add_scalar(f"Loss/train", global_loss, global_step)
             loss = optimizer.loss_scale(loss)
             loss.backward()
             grad_norm = bmt.optim.clip_grad_norm(optimizer.param_groups,max_norm= float('inf'), scale = optimizer.scale, norm_type = 2)
             # bmt.optim_step(optimizer, lr_scheduler)
             bmt.optim_step(optimizer) # fixed learning rate
-            if step % log_iter == 0:
+            if step % args.inspect_iters == 0:
                 bmt.print_rank(
                     "loss: {:.4f} | scale: {:10.4f} | grad_norm: {:.4f} |".format(
                         global_loss,
@@ -179,14 +218,13 @@ def fine_tune():
             pd = [] # prediction
             gt = [] # ground_truth
             for data in val_dataloader:
-                # input_ids = data["input_ids"]
-                # attention_mask = data["attention_mask"]
-                # labels = data["labels"]
                 input_ids, attention_mask, labels = data
+                input_ids = input_ids.cuda()
+                attention_mask = attention_mask.cuda()
+                labels = labels.cuda()
                 logits = model(input_ids, attention_mask)
                 loss = loss_func(logits.view(-1, logits.shape[-1]), labels.view(-1))
                 logits = logits.argmax(dim=-1)
-
                 pd.extend(logits.cpu().tolist())
                 gt.extend(labels.cpu().tolist())
 
@@ -196,29 +234,31 @@ def fine_tune():
 
             # calculate metric
             acc = accuracy_score(gt, pd)
+            if bmt.rank()==0:
+                writer.add_scalar(f"Acc/dev", acc, epoch)
             early_stopping += 1
             bmt.print_rank(f"validation accuracy: {acc*100:.2f}\n")
             if acc > best_valid_acc:
                 best_valid_acc = acc
                 bmt.print_rank("saving the new best model...\n") # save checkpoint
-                torch.save(model.state_dict(), CHECKPOINT_PATH)
+                bmt.save(model, os.path.join(args.save, 'model.pt'))
                 early_stopping = 0 
             valid_acc_list.append(acc)
-            if early_stopping > 5:
-                bmt.print_rank("Accuracy have not rising for 5 epochs.Early stopping..")
-                break # break for iter
-    # 保存valid accuracy变化
-    # df = pd.DataFrame(valid_acc_list)
-    # df.to_csv(SAVED_PATH)
-    bmt.print_rank(f"Validation accuracy is {acc}")
+            # if early_stopping > 5:
+            #     bmt.print_rank("Accuracy have not rising for 5 epochs.Early stopping..")
+            #     break # break for iter
 
 def check_performance():
+    bmt.load(model, os.path.join(args.save, 'model.pt'))
     bmt.print_rank("Checking performance...\n")
     with torch.no_grad():
         pd = [] # prediction
         gt = [] # ground_truth
         for data in test_dataloader:
             input_ids, attention_mask, labels = data
+            input_ids = input_ids.cuda()
+            attention_mask = attention_mask.cuda()
+            labels = labels.cuda()
             logits = model(input_ids, attention_mask)
             loss = loss_func(logits.view(-1, logits.shape[-1]), labels.view(-1))
             logits = logits.argmax(dim=-1)
